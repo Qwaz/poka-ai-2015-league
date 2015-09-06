9b6fc6a0b60f97eff25ee611f18b7a02acfbfdaa
Qwaz <qwazpia@gmail.com>
2015-09-05 17:59:23 -0700

    Generate match results

--- analyzer.py
+++ analyzer.py
@@ -4,9 +4,11 @@ import os.path
 import subprocess
 import time
 
+
 def parse_setting():
     setting_file = open('setting.json', 'r')
     return json.loads(setting_file.read())
+
 
 setting = parse_setting()
 
@@ -21,12 +23,22 @@ for team in setting['ai']:
     if not os.path.exists(ai_path):
         raise FileNotFoundError('파일이 존재하지 않습니다: ' + ai_path)
 
+result = {
+    "ai_list": [team[0] for team in setting['ai']],
+    "match": []
+}
+
 for index1, team1 in enumerate(setting['ai']):
     for index2, team2 in enumerate(setting['ai']):
         if index1 >= index2:
             continue
+        current = {
+            "team1": team1[0],
+            "team2": team2[0],
+            "result": []
+        }
 
-        for match in range(5):
+        for i in range(5):
             time.sleep(1)
             server_process = subprocess.Popen(os.path.join(setting['server_directory'], 'HAJE.SlimeAI.exe'),
                                               cwd=setting['server_directory'])
@@ -40,11 +52,17 @@ for index1, team1 in enumerate(setting['ai']):
             count = [0, 0, 0]
             while True:
                 line = team1_process.stdout.readline()
-                result = re.match(br'Result: (\d+) / (\d+) / (\d+)', line)
-                if result:
+                match = re.match(br'Result: (\d+) / (\d+) / (\d+)', line)
+                if match:
                     for i in range(3):
-                        count[i] = int(result.group(i + 1))
+                        count[i] = int(match.group(i + 1))
                     break
 
             print('%s %d vs %s %d' % (team1[0], count[0], team2[0], count[1]))
+            current['result'].append(count)
             server_process.kill()
+
+        result['match'].append(current)
+
+result_file = open('result.json', 'w')
+json.dump(result, result_file)
